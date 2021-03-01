3fe2d2a6a75a2073b67aeb0767657f6f8628fe26
Valdemar99 <valdemar_wendelboe.andersen.7012@student.lu.se>
2021-03-01 17:44:38 +0100

    Improved error handling

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,30 +12,31 @@ namespace ConsoleClient
         static void Main(string[] args)
         {
             WebService1 webService1 = new WebService1();
-            Building[] buildingList = webService1.ShowBuildings();
-            foreach (Building tmpBuilding in buildingList)
+            Building[] buildingList = new Building[0];
+            Office[] officeList = new Office[0];
+            try
             {
-                Console.WriteLine(tmpBuilding.Address);
-                
+                buildingList = webService1.ShowBuildings();
+                officeList = webService1.ShowOffices();
 
-            }
-
-            Office[] officeList = webService1.ShowOffices();
-            foreach (Office tmpOffice in officeList)
-             
-            {
-                    
-             Console.WriteLine(tmpOffice.BuildingAddress + "\t" + tmpOffice.OfficeNumber + "\t" + tmpOffice.TemperatureSetting + "\t" + tmpOffice.VentilationSetting);
-                
-
-             }
-
-
-           
-                  Console.ReadLine();
+                Console.WriteLine("Buildings:");
+                foreach (Building tmpBuilding in buildingList)
+                {
+                    Console.WriteLine(tmpBuilding.Address);
+                }
+                Console.WriteLine("Offices:");
+                foreach (Office tmpOffice in officeList)
+                {
+                    Console.WriteLine(tmpOffice.BuildingAddress + "\t" + tmpOffice.OfficeNumber + "\t" + tmpOffice.TemperatureSetting + "\t" + tmpOffice.VentilationSetting);
                 }
 
-
             }
+            catch (WebException)
+            {
+                Console.WriteLine("Please check your connection and try again.");
+            }
+            Console.ReadLine();
+        }
+    }
 }
 
